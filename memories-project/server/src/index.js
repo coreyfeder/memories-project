1b04d40a402f5818064d70c26102158cc50e342c
Corey Feder <corey.feder@gmail.com>
2024-04-05 15:17:52 -0400

    that was wrong

--- memories-project/server/src/index.js
+++ memories-project/server/src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 import cors from "cors";
 import connectDB from "./config/db.js";
 
@@ -9,13 +9,14 @@ const app = express();
 
 app.use("/posts", postRoutes)
 
-connectDB()
-
 // app.use(bodyParser.json({ limit: "30mb", exteneded: true }));
 // app.use(bodyParser.urlencoded({ limit: "30mb", exteneded: true }));
+app.use(express.urlencoded({ limit: '30mb' }))
 app.use(express.json({ limit: "30mb" }));
 app.use(cors());
 app.use('/posts', postRoutes)
+
+connectDB()
 
 // const CONNECTION_URL =
 //   "mongodb+srv://ariestitt:mokube123@cluster0.jozbiey.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
